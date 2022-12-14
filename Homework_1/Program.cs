90382665858f5fb51ca3f078b49976cad65c1455
Falcon3590 <118375336+Falcon3590@users.noreply.github.com>
2022-12-14 23:12:28 +0300

    Homework_1

--- Homework_1/Program.cs
+++ Homework_1/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+
+
+
+int [] array = new int[10];
+int result = 0;
+
+for (int i = 0; i <= 9; i++)
+
+{
+
+Random rnd = new Random(); 
+    
+int N = rnd.Next(100, 999); 
+    
+array[i] = N;  
+    
+ 
+    if (array[i] % 2 == 0)
+    {
+        result += 1; 
+    }
+                       
+}
+
+Console.WriteLine($"Количество четных чисел в массиве = {result}");
+
+
+  
